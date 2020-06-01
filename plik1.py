0c0c85eb8c8ef0a8257c8dca092a78b0f62dcee4
Lukasz Drwal <drwallukasz@gmail.com>
2020-06-01 21:02:54 +0100

    modyfikacje w plikach 1-2

--- plik1.py
+++ plik1.py
@@ -5,6 +5,7 @@
 def func_print_range(value=1):
     for i in range(value):
         print(i)
+        print('tekst1')
 
 
 if __name__ == '__main__':
--- plik2.py
+++ plik2.py
@@ -7,4 +7,5 @@ if __name__ == '__main__':
     print(f'Wyswietl liczbe {int(45)}')
     print('tekst3')
     print('tekst4')
+    print('tekst6')
 
